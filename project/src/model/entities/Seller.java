ab71b4f2ad8202988157741f51d5bb3b6de607e9
Carlos Alexandre <contatocarlosalexandrecb@gmail.com>
2024-04-18 02:03:13 -0300

    Atualização geral
    Atualizado todas os packages, classes e interfaces.

--- project/src/model/entities/Seller.java
+++ project/src/model/entities/Seller.java
@@ -6,23 +6,25 @@ import java.util.Objects;
 
 public class Seller implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private Integer id;
     private String name;
     private String email;
-    private Date birthDay;
-    private Double baseSalay;
+    private Date birthDate;
+    private Double baseSalary;
 
     private Department department;
 
     public Seller() {
     }
 
-    public Seller(Integer id, String name, String email, Date birthDay, Double baseSalay, Department department) {
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.birthDay = birthDay;
-        this.baseSalay = baseSalay;
+        this.birthDate = birthDate;
+        this.baseSalary = baseSalary;
         this.department = department;
     }
 
@@ -50,20 +52,20 @@ public class Seller implements Serializable {
         this.email = email;
     }
 
-    public Date getBirthDay() {
-        return birthDay;
+    public Date getBirthDate() {
+        return birthDate;
     }
 
-    public void setBirthDay(Date birthDay) {
-        this.birthDay = birthDay;
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
     }
 
-    public Double getBaseSalay() {
-        return baseSalay;
+    public Double getBaseSalary() {
+        return baseSalary;
     }
 
-    public void setBaseSalay(Double baseSalay) {
-        this.baseSalay = baseSalay;
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
     }
 
     public Department getDepartment() {
@@ -75,27 +77,33 @@ public class Seller implements Serializable {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Seller seller = (Seller) o;
-        return Objects.equals(id, seller.id);
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Seller other = (Seller) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 
     @Override
     public String toString() {
-        return "Seller{" +
-                "id = " + id +
-                ", name = '" + name + '\'' +
-                ", email = '" + email + '\'' +
-                ", birthDay = " + birthDay +
-                ", baseSalay = " + baseSalay +
-                ", department = " + department +
-                '}';
+        return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+                + baseSalary + ", department=" + department + "]";
     }
 }
